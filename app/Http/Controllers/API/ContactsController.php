9d0401457ef1a12b774c4bc8370bb5a1270fcaae
Rohit <patil.rohit048@gmail.com>
2018-09-22 17:34:16 +0530

    Contacts are done. Almost...

--- app/Http/Controllers/API/ContactsController.php
+++ app/Http/Controllers/API/ContactsController.php
@@ -14,7 +14,7 @@ class ContactsController extends Controller
     {
         $contact = Contact::all();
 
-        return ContactResource :: collection($contact);
+        return $contact;
     }
 
 
@@ -46,8 +46,14 @@ class ContactsController extends Controller
 
     public function show($id)
     {
-        //
+
+        $contact = Contact::where('id', $id)->get();
+        if($contact){
+            return $contact;
+        }
+        return response()->json(['error' => 'Data not found'], 404);
     }
+
 
 
     public function edit($id)
@@ -58,12 +64,35 @@ class ContactsController extends Controller
 
     public function update(Request $request, $id)
     {
-        //
+        $contact = Contact::find($id);
+
+        if($company==null){
+            return response()->json(['error' => 'Data not found'], 404);
+        }
+
+        //$comp = $company::create($request->except('_token'));
+        $contact->name = $request->input('name');
+        $contact->designation = $request->input('designation');
+        $contact->mobile_no = $request->input('mobile_no');
+        $contact->landline_no = $request->input('landline_no');
+        $contact->email = $request->input('email');
+        $contact->company_id = $request->input('company_id');
+        $contact->branch_id = $request->input('branch_id');
+
+        if($contact->save()){
+          return new ContactResource($contact);
+        }
+        return response()->json(['error' => 'User is not allowed to make chnages'], 403);
     }
 
 
     public function destroy($id)
     {
-        //
+        $contact = Contact::findOrFail($cid);
+
+        if($company->delete()){
+          return response()->json(['Result' => 'Suceess'], 200);
+        }
+        return response()->json(['error' => 'Something went wrong'], 500)
     }
 }
